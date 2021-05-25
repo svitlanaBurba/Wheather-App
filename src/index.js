a946124246350755014bbdd7e8f327868c2d21ae
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-05-25 16:21:21 +0300

    templates

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './sass/main.scss';
+import tDivTpl from '../templates/t-div.hbs';
+import loadMoreTpl from '../templates/load-more.hbs';
+import chartDivTpl from '../templates/chart-div.hbs';
